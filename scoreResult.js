88038187db5a1b40b1780dacf14d9a2b07751136
kutaku <kt05801623@gmail.com>
2023-01-11 18:55:07 +0900

    add 13600K price

--- scoreResult.js
+++ scoreResult.js
@@ -180,27 +180,27 @@ function onRadioButtonChange() {
             target.innerHTML = "<h3>13600K＆3060 SCORE</h3><br>" +
                 "<h2><span><b>◆" + (score13600k + score3060) + "</b>：合計性能スコア</span></h2>" +
                 "<span>passmark " + "Core i5 13600K: " + score13600k + " + RTX3060 12GB: " + score3060 + "</span><br>" +
-                "<span>・お勧め構成：メモリ32GB / SSD1TB / マザーボードZ690 / 電源GOLD750W <br>・BTO推定価格：22万円前後 <br>・フルHD120Hzゲーミングにおすすめ！</span><br/>";
+                "<span>・お勧め構成：メモリ32GB / SSD1TB / マザーボードZ790 / 電源GOLD750W <br>・BTO推定価格：27万円前後 <br>・ゲーム配信 / フルHD120Hzゲーミングにおすすめ！</span><br/>";
         } else if (gpubtn3060ti.checked == true) {
             target.innerHTML = "<h3>13600K＆3060Ti SCORE</h3><br>" +
                 "<h2><span><b>◆" + (score13600k + score3060ti) + "</b>：合計性能スコア</span></h2>" +
                 "<span>passmark " + "Core i5 13600K: " + score13600k + " + RTX3060Ti 8GB: " + score3060ti + "</span><br>" +
-                "<span>・お勧め構成：メモリ32GB / SSD1TB / マザーボードZ690 / 電源GOLD750W <br>・BTO推定価格：24万円前後 <br>・フルHD144Hzゲーミングにおすすめ！</span><br/>";
+                "<span>・お勧め構成：メモリ32GB / SSD1TB / マザーボードZ790 / 電源GOLD750W <br>・BTO推定価格：28万円前後 <br>・ゲーム配信 / フルHD165Hzゲーミングにおすすめ！</span><br/>";
         } else if (gpubtn3070.checked == true) {
             target.innerHTML = "<h3>13600K＆3070 SCORE</h3><br>" +
                 "<h2><span><b>◆" + (score13600k + score3070) + "</b>：合計性能スコア</span></h2>" +
                 "<span>passmark " + "Core i5 13600K: " + score13600k + " + RTX3070 8GB: " + score3070 + "</span><br>" +
-                "<span>・お勧め構成：メモリ32GB / SSD1TB / マザーボードZ690 / 電源GOLD750W <br>・BTO推定価格：25万円前後 <br>・フルHD180Hz / WQHD120Hzゲーミングにおすすめ！</span><br/>";
+                "<span>・お勧め構成：メモリ32GB / SSD1TB / マザーボードZ790 / 電源GOLD750W <br>・BTO推定価格：32万円前後 <br>・ゲーム配信 / フルHD240Hz / WQHD120Hzゲーミングにおすすめ！</span><br/>";
         } else if (gpubtn3070ti.checked == true) {
             target.innerHTML = "<h3>13600K＆3070Ti SCORE</h3><br>" +
                 "<h2><span><b>◆" + (score13600k + score3070ti) + "</b>：合計性能スコア</span></h2>" +
                 "<span>passmark " + "Core i5 13600K: " + score13600k + " + RTX3070Ti 8GB: " + score3070ti + "</span><br>" +
-                "<span>・お勧め構成：メモリ32GB / SSD1TB / マザーボードZ690 / 電源GOLD750W <br>・BTO推定価格：26万円前後 <br>・フルHD240Hz / WQHD144Hz ゲーミングにおすすめ！</span><br/>";
+                "<span>・お勧め構成：メモリ32GB / SSD1TB / マザーボードZ790 / 電源GOLD750W <br>・BTO推定価格：33万円前後 <br>・ゲーム配信 / フルHD280Hz / WQHD144Hz ゲーミングにおすすめ！</span><br/>";
         } else if (gpubtn3080.checked == true) {
             target.innerHTML = "<h3>13600K＆3080 SCORE</h3><br>" +
                 "<h2><span><b>◆" + (score13600k + score3080) + "</b>：合計性能スコア</span></h2>" +
                 "<span>passmark " + "Core i5 13600K: " + score13600k + " + RTX3080 10GB: " + score3080 + "</span><br>" +
-                "<span>・お勧め構成：メモリ32GB / SSD1TB / マザーボードZ690 / 電源GOLD850W <br>・BTO推定価格：30万円前後 <br>・WQHD165Hzゲーミングにおすすめ！</span><br/>";
+                "<span>・お勧め構成：メモリ32GB / SSD1TB / マザーボードZ790 / 電源GOLD850W <br>・BTO推定価格：37万円前後 <br>・ゲーム配信 / フルHD360Hz /  WQHD165Hzゲーミングにおすすめ！</span><br/>";
         }
     }
     else if (cpubtn13700.checked == true) {
