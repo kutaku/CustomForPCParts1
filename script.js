7a4991b65011881e19d8c0b1ff7a95d47a929a1b
kutaku <kt05801623@gmail.com>
2023-01-11 21:31:40 +0900

    hide score display button

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-$(function(){
+$(function () {
 
-    // SNSボタン
-    $('.social-icon').hover(
-      function(){
-        $(this).children('span').animate({
-          'font-size':'30px'
-        }, 300);
-      },
-      function(){
-        $(this).children('span').animate({
-          'font-size':'24px'
-        }, 300);
-      }
-    );
-    
-    // トップへ戻るボタン
-    $('#top-btn').click(function(){
-      $('html,body').animate({ 
-        'scrollTop': 0 
-      }, 'slow');
-    });
-    
-    // ヘッダー内の<a>タグをクリックしたときclickイベントを作成
-    $('header a').click(function(){
-     var id = $(this).attr('href');
-     var position = $(id).offset().top;
-     $('html,body').animate({
-       'scrollTop': position
-      
-     }, 500);
-    });
+  // SNSボタン
+  $('.social-icon').hover(
+    function () {
+      $(this).children('span').animate({
+        'font-size': '30px'
+      }, 300);
+    },
+    function () {
+      $(this).children('span').animate({
+        'font-size': '24px'
+      }, 300);
+    }
+  );
 
-
+  // トップへ戻るボタン
+  $('#top-btn').click(function () {
+    $('html,body').animate({
+      'scrollTop': 0
+    }, 'slow');
   });
+
+  // ヘッダー内の<a>タグをクリックしたときclickイベントを作成
+  $('header a').click(function () {
+    var id = $(this).attr('href');
+    var position = $(id).offset().top;
+    $('html,body').animate({
+      'scrollTop': position
+
+    }, 500);
+  });
+
+
+});
